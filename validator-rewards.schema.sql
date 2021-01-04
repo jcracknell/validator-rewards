dd58505c086e04045f5cab20465e0b055882021a
James Cracknell <james.h.cracknell@gmail.com>
2021-01-04 14:44:10 -0700

    Views aggregate by validator

--- validator-rewards.schema.sql
+++ validator-rewards.schema.sql
@@ -27,34 +27,46 @@ create table if not exists "ValidatorReward" (
   constraint "FK_ValidatorReword_Validator" foreign key ("validator_index") references "Validator" ("index")
 ) without rowid;
 
-drop view if exists "DailyReward";
-create view "DailyReward" as
+drop view if exists "DailyValidatorReward";
+create view "DailyValidatorReward" as
 select
-  "g"."day",
-  sum("g"."reward") / 1000000000.0 as "reward_eth",
-  sum("g"."reward") as "reward_gwei"
+  "r"."day",
+  "r"."validator_index",
+  "v"."pubkey",
+  sum("r"."reward") / 1000000000.0 as "reward_eth",
+  sum("r"."reward") as "reward_gwei"
 from
   (
-    select strftime('%Y-%m-%d', "e"."end_time", 'unixepoch', 'localtime') as "day", "vr"."reward"
+    select
+      strftime('%Y-%m-%d', "e"."end_time", 'unixepoch', 'localtime') as "day",
+      "vr"."validator_index",
+      "vr"."reward"
     from "Epoch" as "e"
     join "ValidatorReward" as "vr" on "vr"."epoch_index" = "e"."index"
-  ) as "g"
-group by "g"."day"
-order by "g"."day" asc;
+  ) as "r"
+  join "Validator" as "v" on "v"."index" = "r"."validator_index"
+group by "r"."day", "r"."validator_index", "v"."pubkey"
+order by "r"."day" asc, "r"."validator_index" asc;
 
-drop view if exists "MonthlyReward";
-create view "MonthlyReward" as
+drop view if exists "MonthlyValidatorReward";
+create view "MonthlyValidatorReward" as
 select
-  "g"."month",
-  sum("g"."reward") / 1000000000.0 as "reward_eth",
-  sum("g"."reward") as "reward_gwei"
+  "r"."month",
+  "r"."validator_index",
+  "v"."pubkey",
+  sum("r"."reward") / 1000000000.0 as "reward_eth",
+  sum("r"."reward") as "reward_gwei"
 from
   (
-    select strftime('%Y-%m', "e"."end_time", 'unixepoch', 'localtime') as "month", "vr"."reward"
+    select
+      strftime('%Y-%m', "e"."end_time", 'unixepoch', 'localtime') as "month",
+      "vr"."validator_index",
+      "vr"."reward"
     from "Epoch" as "e"
     join "ValidatorReward" as "vr" on "vr"."epoch_index" = "e"."index"
-  ) as "g"
-group by "g"."month"
-order by "g"."month" asc;
+  ) as "r"
+  join "Validator" as "v" on "v"."index" = "r"."validator_index"
+group by "r"."month", "r"."validator_index", "v"."pubkey"
+order by "r"."month" asc, "r"."validator_index" asc;
 
 end transaction;
